94618e59455539bf0451a996aaab5b6425942c21
bytemallet <articulosinfosec@gmail.com>
2025-07-18 19:45:01 +0200

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hack4fun-security-hub/",
   server: {
     host: "::",
     port: 8080,
